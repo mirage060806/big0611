4b5e285d7f27843b607d3dac251c491196c30fcb
m <mirage060806@naver.com>
2026-07-23 13:46:37 +0900

    0723

--- py/p02_데이터분석/ch08_streamlit/my_first_app.py
+++ py/p02_데이터분석/ch08_streamlit/my_first_app.py
@@ -57,3 +57,52 @@ color = st.radio(
 agree = st.checkbox('이용약관에 동의합니다.')
 if agree:
     st.write('동의해주셔서 감사합니다.')
+
+# 5. 여러 개 선택하기
+hobbies = st.multiselect(
+    '취미를 선택하세요' ('여러 개 선택 가능'),
+    ['독서', '영화감상', '운동', '여행', '음악감상']
+)
+
+if hobbies:
+    st.write('선택한 취미:', hobbies)
+
+# 6. 날짜와 시간 입력
+today = st.date_input('날짜를 선택하세요')
+current_time = st.time_input('시간을 선택하세요')
+
+st.write(f'선택한 날짜: {today}')
+st.write(f'선택한 시간: {current_time}')
+
+
+# 미디어 컴포넌트
+# 1. 이미지 표시하기 <img src="경로/파일명" width=300 alt="대체텍스트">
+# 비트맵(jpg, gif, png)
+# 백터(svg)
+# 인터넷 이미지 표시
+# st.image('https://strage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/ba819fd9-0cbb-401a-9136-9fe3fcb23111/Home_Page.png', caption='예시 이미지')
+# https://picsum.photos -> 더미이미지
+# https://unsplash.com/ko -> 사진
+# https://www.pngwing.com/ko -> 심볼, 아이콘
+st.image('https://picsum.photos/seed/picsum/300/200/?blur', caption='인터넷 이미지')
+st.image('https://picsum.photos/id/237/300/200', caption='인터넷 이미지')
+
+# 로컬 이미지 파일 표시 (파일이 있는 경우)
+st.image('my_image.jpg', caption='내 이미지')
+
+# AI 영상
+
+# 비디오 파일 재생
+# https://www.pexels.com/ko-kr/
+# mp4, webm, ogv(ogg)
+# st.video
+
+# 유튜브 비디오 표시 <iframe src="경로/파일명"></iframe>
+# YouTube
+# st.video
+
+
+# 오디오 파일 재생
+# mp3(노래), ogg(ogv), wav(미디음)
+# YouTube audio library
+st.audio('my_audio.mp3')
